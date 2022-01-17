efffd950dd87d8db1757fa8a0c47c1b2126d0296
Niv Kaminer <nivkner@zoho.com>
2022-01-17 21:27:17 +0200

    maintain less state, and set at construction data that doesn't change

--- Pathfinder.py
+++ Pathfinder.py
@@ -8,14 +8,12 @@ def distance(p0: Position, p1: Position):
     return math.sqrt(pow(p0.x_m-p1.x_m, 2)+pow(p0.y_m-p1.y_m, 2)+pow(p0.z_m-p1.z_m, 2))
 
 class Pathfinder:
-    def __init__(self):
-        self.client = None
-        self.goal = None
-        self.drone_speed = 0
-        self.max_goal_distance = 0
-        self.following_boundary = False
+    def __init__(self, client: DroneClient, goal: Position):
+        self.client = client
+        self.goal = goal
+        self.drone_speed = 0.0
+        self.max_goal_distance = 0.0
         self.min_boundary_distance = float("inf")
-        self.leave_distance = None
 
     def setMaxGoalDistance(self, max_goal_distance: float):
         self.max_goal_distance = max_goal_distance
@@ -23,20 +21,12 @@ class Pathfinder:
     def setDroneSpeed(self, drone_speed: float):
         self.drone_speed = drone_speed
 
-    def setClient(self, client: DroneClient):
-        self.client = client
-
-    def setGoal(self, goal: Position):
-        self.goal = goal
-
     def motionToGoal(self):
-        self.following_boundary = False
-        self.client.flyToPosition(self.goal.x_m, self.goal.y_m, self.goal.z_m, self.drone_speed)
+        self.client.flyToPosition(
+            self.goal.x_m, self.goal.y_m, self.goal.z_m, self.drone_speed)
 
     def followBoundary(self):
-        self.following_boundary = True
-        if self.min_boundary_distance > self.leave_distance:
-            self.motionToGoal()
+        pass
 
     def pathfind(self):
         pose = self.client.getPose()
@@ -52,7 +42,7 @@ class Pathfinder:
         self.leave_distance = min_heuristic_distance
         min_heuristic_point = point_cloud[idx_min_heuristic_distance]
 
-        if self.following_boundary:
+        if self.min_boundary_distance <= min_heuristic_distance:
             self.followBoundary()
         else:
             self.motionToGoal()
